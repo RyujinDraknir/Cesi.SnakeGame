99e561b9d034d6cf307a3f8cbd569107bccf1596
Arthur Laroque <laroque.e1903405@etud.univ-ubs.fr>
2021-12-09 14:47:42 +0100

    merde

---
+++ Model/DbAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cesi.SnakeGame.Model
+{
+    public class DbAccess
+    {
+        private static DbAccess Instance;
+
+
+        private IEnumerable<User> Users;
+
+        public DbAccess()
+        {
+            LoadData();
+        }
+
+        public static DbAccess GetInstance()
+        {
+            if (Instance == null)
+                Instance = new DbAccess();
+            return Instance
+        }
+
+        public void LoadData()
+        {
+
+        }
+    }
+}
---
+++ Model/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cesi.SnakeGame.Model
+{
+    public class Game
+    {
+        public Game(User user)
+        {
+            User = user;
+        }
+
+        private User User { get; set; }
+    }
+}
